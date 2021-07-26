eaa89e0055f63c5e2a8fb7ccf5427bc270fa69e2
Chris <paul19992012@gmail.com>
2021-07-25 19:50:36 -0500

    logica basica

---
+++ backward chaining/models/Done.js
@@ -0,0 +1,6 @@
+export class Done {
+    constructor(predicado, sujetos) {
+        this.predicado = predicado;
+        this.sujetos = sujetos;
+    }
+}
---
+++ backward chaining/models/Rule.js
@@ -0,0 +1,8 @@
+export class Rule {
+    constructor(id, predicado, sujetos, condicion) {
+        this.id = id;
+        this.predicado = predicado;
+        this.sujetos = sujetos;
+        this.condicion = condicion;
+    }
+}
