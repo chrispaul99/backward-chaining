feaa7aa89d928577798621c233e8b5db8be55ef5
Chris <paul19992012@gmail.com>
2021-07-26 21:59:41 -0500

    register rules and facts

--- api-simulador-prolog/functions/lib/models/Rule.js
+++ api-simulador-prolog/functions/lib/models/Rule.js
@@ -1,12 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Rule = void 0;
+const Fact_1 = require("./Fact");
 class Rule {
     constructor() {
         this.id = "";
-        this.predicado = "";
-        this.sujetos = [];
-        this.condicion = [];
+        this.conclusion = new Fact_1.Fact();
+        this.condiciones = [];
+        this.operadores = [];
     }
 }
 exports.Rule = Rule;
